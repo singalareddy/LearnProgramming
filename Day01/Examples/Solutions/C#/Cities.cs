e2b59efa1e0be94cd9a974fc85b2cb84098cb7d4
Prasanth Reddy <prasanth.maddala@valuelabs.com>
2020-12-16 16:22:34 +0530

    Day 1 - Hello World and Escape sequences

---
+++ Day01/Examples/Solutions/C#/Cities.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Anantapur,\nNellore,\nKadapa,\nKurnool,\nChittor");
+    }
+}
---
+++ Day01/Examples/Solutions/C#/FamilyNames.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("MyName: M. Prasanth Reddy,");
+        Console.WriteLine("Father: M. RosiReddy,");
+        Console.WriteLine("Mother: M. Annapurna");
+    }
+}
