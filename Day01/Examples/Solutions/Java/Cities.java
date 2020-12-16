432e87e1fbd832f64890f70fa3f080b4ce00f503
Prasanth Reddy <prasanth.maddala@valuelabs.com>
2020-12-16 16:10:30 +0530

    Day 1 - Hello World and Escape sequences

---
+++ Day01/Examples/Solutions/Java/Cities.java
@@ -0,0 +1,6 @@
+public class Cities{
+
+     public static void main(String []args){
+        System.out.println("Anantapur,\nNellore,\nKadapa,\nKurnool,\nChittor");
+     }
+}
---
+++ Day01/Examples/Solutions/Java/FamilyNames.java
@@ -0,0 +1,8 @@
+public class FamilyNames{
+
+     public static void main(String []args){
+        System.out.println("MyName: M. Prasanth Reddy,");
+        System.out.println("Father: M. RosiReddy,");
+        System.out.println("Mother: M. Annapurna");
+     }
+}
